56f905a79c0fc0c487b7227425f9e7d4903b7e0d
Khalif <anupamborboruah100@gmail.com>
2023-02-22 19:59:58 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
+import React,{useState} from 'react';
 import './App.css';
-import {useState} from "react";
 
-function App() {
-    // Counter
-    const [number, setNumber] = useState(10);
 
-    // Reduce Counter
-    const onRemove = () => {
-        setNumber(number - 1);
-    }
+const App=()=>{
 
-    // Increase Counter
-    const onAdd = () => {
-        setNumber(number + 1);
-    }
+  const [result,setResult]=useState("");
 
-    return (
-        <div className="App">
-            <div className="plus" onClick={() => {
-                onAdd();
-            }}> +
-            </div>
-            <div className="value"> {number} </div>
-            <div className="minus" onClick={() => {
-                onRemove()
-            }}> -
-            </div>
-        </div>
-    );
+  const clickHandler=(event)=>{
+    setResult(result.concat(event.target.value))  //to change the displayed data and combine the previous result withe the new result
+  }
+
+  const clearDisplay=()=>{
+
+    setResult("");   //to clear the display
+
+  }
+
+  const calculate=()=>{
+    // eslint-disable-next-line no-eval
+    setResult(eval(result),toString());   //eval is a function//
+  }
+
+  return(
+
+
+    // body of the calculator
+
+
+    <div className="calc">
+      <input type="text" placeholder="0" id="answer" value={result} />                 {/* display portion */}
+      <input type="button" value="9" className="button" onClick={clickHandler}/>       {/* buttons */}
+      <input type="button" value="8" className="button" onClick={clickHandler}/>
+      <input type="button" value="7" className="button" onClick={clickHandler}/>
+      <input type="button" value="+" className="button" onClick={clickHandler}/>
+      <input type="button" value="6" className="button" onClick={clickHandler}/>
+      <input type="button" value="5" className="button" onClick={clickHandler}/>
+      <input type="button" value="4" className="button" onClick={clickHandler}/>
+      <input type="button" value="-" className="button" onClick={clickHandler}/>
+      <input type="button" value="3" className="button" onClick={clickHandler}/>
+      <input type="button" value="2" className="button" onClick={clickHandler}/>
+      <input type="button" value="1" className="button" onClick={clickHandler}/>
+      <input type="button" value="*" className="button" onClick={clickHandler}/>
+      <input type="button" value="0" className="button" onClick={clickHandler}/>
+      <input type="button" value="." className="button" onClick={clickHandler}/>
+      <input type="button" value="%" className="button" onClick={clickHandler}/>
+      <input type="button" value="/" className="button" onClick={clickHandler}/>
+      <input type="button" value="clear" className="button button1" onClick={clearDisplay} />
+      <input type="button" value="=" className="button button1" onClick={calculate}/>
+    
+    </div>
+  )
 }
 
 export default App;
